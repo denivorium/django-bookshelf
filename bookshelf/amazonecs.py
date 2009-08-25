fe08efd7da4f13676d27baaeb4437a31a37b606d
Cosmin Stejerean <cosmin@offbytwo.com>
2009-08-25 11:19:29 -0500

    Started to add logic for signing requests based on new ECS API requirements

--- bookshelf/amazonecs.py
+++ bookshelf/amazonecs.py
@@ -1,3 +1,6 @@
+import base64
+from datetime import datetime
+import hashlib, hmac
 import urllib2, urllib
 from lxml import etree
 
@@ -5,17 +8,34 @@ from django.conf import settings
 
 ECS_URL = "http://ecs.amazonaws.com/onca/xml"
 
+def encode_query(querydict):
+    return urllib.urlencode(querydict).replace('+', '%20').replace('%7E', '~')
+
+def sign_request(httpverb, hostname, requesturi, queryparams):
+    tosign = '\n'.join((httpverb, hostname, requesturi, encode_query(queryparams)))
+    return base64.encodestring(hmac.new(settings.AWS_SECRET_KEY, tosign, hashlib.sha256).digest())[:-1]
+
 def lookup(item_id, id_type):
+    httpverb = 'GET'
+    hostname = 'ecs.amazonaws.com'
+    requesturi = '/onca/xml'
     params = {"Service" : "AWSECommerceService",
               "Version" : settings.AWS_ECS_VERSION,
               "Operation" : "ItemLookup",
-              "SubscriptionId" : settings.AWS_ACCESS_KEY,
+              "AWSAccessKeyId" : settings.AWS_ACCESS_KEY,
               "SearchIndex" : "Books",
               "ItemId" : item_id,
               "IdType" : id_type,
-              "ResponseGroup" : "Images,ItemAttributes,ItemIds,OfferSummary,Offers,Small"}
+              "ResponseGroup" : "Images,ItemAttributes,ItemIds,OfferSummary,Offers,Small",
+              'Timestamp' : datetime.utcnow().isoformat().split('.')[0] + ".000Z"}
 
-    url  = ECS_URL + "?" + urllib.urlencode(params)
+    signature = sign_request(httpverb, hostname, requesturi, params)
+    params.update({'Signature': signature})
+    querystring = encode_query(params)
+
+    url = 'http://' + hostname + requesturi + '?' + querystring
+    print 'Signed URL is ', url
+
     r = etree.fromstring(urllib2.urlopen(url).read())
 
     nsmap = {'ecs' : r.nsmap[None]}
@@ -27,5 +47,4 @@ def lookup(item_id, id_type):
             return None
     else:
         raise ValueError('Request is not valid, full response %s' % etree.tostring(r))
-
 
