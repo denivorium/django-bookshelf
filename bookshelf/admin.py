6ce7fd851c515333db14c9eacfbbfa82411fc90c
Cosmin Stejerean <cosmin@offbytwo.com>
2009-08-24 23:22:00 -0500

    fixed the search fields for books

--- bookshelf/admin.py
+++ bookshelf/admin.py
@@ -5,6 +5,6 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'isbn', 'list_price', 'amazon_price',
                     'new_price', 'used_price', 'image', 'detail_page')
 
-    search_fields = ('title')
+    search_fields = ('title',)
 
 admin.site.register(models.Book, BookAdmin)
