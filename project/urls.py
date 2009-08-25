825633d00e8a51557a9d027c6cad98e0d17b9002
Cosmin Stejerean (offbytwo) <cosmin@offbytwo.com>
2009-08-24 21:44:32 -0500

    added new django project using deadwisdom's best practices

---
+++ project/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^project/', include('project.foo.urls')),
+
+    (r'^admin/', include(admin.site.urls)),
+)
+
+if getattr(settings, 'MEDIA_SERVE', False):
+    urlpatterns = patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    ) + urlpatterns
