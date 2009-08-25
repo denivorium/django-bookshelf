aa20ec9da530cee030916a0a9558b252cb508479
Cosmin Stejerean <cosmin@offbytwo.com>
2009-08-25 11:18:27 -0500

    added url mappings for the login and logout views

--- project/urls.py
+++ project/urls.py
@@ -8,9 +8,13 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Example:
     # (r'^project/', include('project.foo.urls')),
-    (r'^$', include('bookshelf.urls')),
-
     (r'^admin/', include(admin.site.urls)),
+
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout_then_login'),
+    (r'^accounts/profile/$', 'django.views.generic.simple.redirect_to', {'url':'/'}),
+
+    (r'^$', 'bookshelf.views.books'),
 )
 
 if getattr(settings, 'MEDIA_SERVE', False):
